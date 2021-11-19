f6435348303c5a54897e3aa441cf0613fcd01e6f
Toshiyuki Honda <rocket-martue@users.noreply.github.com>
2021-11-19 14:15:52 +0900

    Update rbui-shortcode.php

--- functions/rbui-shortcode.php
+++ functions/rbui-shortcode.php
@@ -1,43 +1,31 @@
 <?php
-
-add_action(
-	'admin_menu',
-	function () {
-		add_menu_page(
-			__( 'Reusable Blocks', 'reusable-blocks-user-interface' ),
-			__( 'Reusable Blocks', 'reusable-blocks-user-interface' ),
-			'edit_posts',
-			'edit.php?post_type=wp_block',
-			'',
-			'dashicons-block-default',
-			22
-		);
-	}
-);
-
-add_filter(
-	'manage_posts_columns',
-	function ( $columns ) {
-		global $post_type;
-		if( in_array( $post_type, array( 'wp_block' ) ) ) {
-			$new_columns = array(
-				'shortcode' => esc_html__( 'Shortcode', 'reusable-blocks-user-interface' ),
-			);
-			return array_merge( $columns, $new_columns );
+/**
+ * shortcode.
+ *
+ * @param array $atts User defined attributes in shortcode tag.
+ *
+ * @return string
+ */
+function rbui_shortcode( $atts ) {
+	extract( shortcode_atts(
+		array(
+			'slug' => '',
+		), $atts ) );
+	ob_start();
+	$args = array(
+		'post_type' => array( 'wp_block' ),
+		'name'      => $slug,
+	);
+	$query = new WP_Query( $args );
+	if ( $query->have_posts() ) {
+		while ( $query->have_posts() ) {
+			$query->the_post();
+			the_content();
 		}
-		return $columns;
 	}
-);
-
-add_filter(
-	'manage_posts_custom_column',
-	function ( $column_name, $post_id ) {
-		if ( 'shortcode' === $column_name ) {
-			$post = get_post( $post_id );
-			$slug = $post->post_name;
-			echo '<span class="rbui-short-code">[rbui slug=' . esc_html( $slug ) . ']</span>';
-		}
-	},
-	10,
-	2
-);
+	wp_reset_postdata();
+	$html = ob_get_contents();
+	ob_end_clean();
+	return $html;
+}
+add_shortcode( 'rbui', 'rbui_shortcode' );
