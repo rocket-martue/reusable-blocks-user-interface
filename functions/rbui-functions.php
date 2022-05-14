46cb1b72dc5615d8b7db8f210ddadc3815f65169
Toshiyuki Honda <rocket-martue@users.noreply.github.com>
2022-05-14 14:38:47 +0900

    Add files via upload

---
+++ functions/rbui-functions.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * the_rubi_content function.
+ * @param mixed $slug The slug of the reusable block.
+ * @return $content The content of the block.
+ */
+function rbui_get( $slug ) {
+	$args = array(
+		'post_type' => array( 'wp_block' ),
+		'name'      => $slug,
+	);
+	$query = new WP_Query( $args );
+	if ( $query->have_posts() ) {
+		while ( $query->have_posts() ) {
+			$query->the_post();
+			$content = get_the_content();
+		}
+	}
+	wp_reset_postdata();
+	return $content;
+}
+function the_rubi_content( $slug ) {
+	echo apply_filters( 'the_content', rbui_get( $slug ) );
+}
