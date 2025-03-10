086a755cd6e35613a14415a87464bb3b6e959ba4
Toshiyuki Honda <rocket.martue@gmail.com>
2025-03-11 02:57:28 +0900

    Revert "Update rbui-shortcode.php"
    This reverts commit 20cd165521f3afc4a5ca099b3e131bc43dade35b.

--- functions/rbui-shortcode.php
+++ functions/rbui-shortcode.php
@@ -11,30 +11,29 @@
  * @param array $atts User defined attributes in shortcode tag.
  * @return string
  */
-function earb_shortcode( $atts ) {
-	$args    = shortcode_atts(
+function rbui_shortcode( $atts ) {
+	$args = shortcode_atts(
 		array(
-			'post_id' => '',
+			'slug' => '',
 		),
 		$atts
 	);
-	$post_id = $args['post_id'];
-	if ( $post_id ) {
-		$args  = array(
-			'post_type' => 'wp_block',
-			'p'         => $post_id,
-		);
-		$query = new WP_Query( $args );
-		if ( $query->have_posts() ) {
-			while ( $query->have_posts() ) {
-				$query->the_post();
-				the_content();
-			}
+	$slug = $args['slug'];
+	ob_start();
+	$args  = array(
+		'post_type' => array( 'wp_block' ),
+		'name'      => $slug,
+	);
+	$query = new WP_Query( $args );
+	if ( $query->have_posts() ) {
+		while ( $query->have_posts() ) {
+			$query->the_post();
+			the_content();
 		}
-		wp_reset_postdata();
-		$html = ob_get_contents();
-		ob_end_clean();
-		return $html;
 	}
+	wp_reset_postdata();
+	$html = ob_get_contents();
+	ob_end_clean();
+	return $html;
 }
 add_shortcode( 'rbui', 'rbui_shortcode' );
